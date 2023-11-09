4bdd5dfe2be0a0b86405bb41f2ca1abab02f6b84
AlexanderZadorozhnyy <alexander.zadorozhnyy@yandex.ru>
2023-11-10 00:05:24 +0300

    Remove comment

--- src/laboratory_worк_one/tests/test.py
+++ src/laboratory_worк_one/tests/test.py
@@ -15,15 +15,3 @@ def test_maze_solver():
 
 if __name__ == "__main__":
     test_maze_solver()
-# def test_maze_solver_big():
-#     n = 1920
-#
-#     temp1 = ["."]
-#     temp2 = ["#"] * (n - 1)
-#     temp1.extend(temp2)
-#     labyrinth = [temp1] * n
-#
-#     maze_solver = Maze_Solver(labyrinth, n)
-#
-#     for i in range(1, 1920):
-#        assert maze_solver.query(0, 0, (n - i, 0), n) == True
